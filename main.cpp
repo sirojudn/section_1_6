5622888fb1da475f39ddb8fb0f6203e01bfe884d
sirojudn <sirojudn@gmail.com>
2017-08-07 10:54:49 +0700

    bookstore exercise 1.6

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "Sales_item.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Sales_item copies;
+
+    if (std::cin >> copies){
+        Sales_item curItem;
+        while (std::cin >> curItem){
+            if (copies.isbn() == curItem.isbn()){
+                copies = copies + curItem;
+            }
+            else {
+                std::cout << copies << std::endl;
+                copies = curItem;
+            }
+        }
+        std::cout << copies << std::endl;
+    }
+    else {
+        return -1;
+    }
+
     return 0;
 }
